4e5c2ca8a800a5aad18b6a75c7c37bfb64017732
liyongchao <liyongchao@corp.rrzcp8.com>
2018-02-26 16:04:33 +0800

    0 1 矩阵距离问题

--- src/main/java/com/lkzlee/leetcode/Solution_542.java
+++ src/main/java/com/lkzlee/leetcode/Solution_542.java
@@ -1,5 +1,6 @@
 package com.lkzlee.leetcode;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -10,14 +11,73 @@ import java.util.Queue;
  * Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
  * The distance between two adjacent cells is 1.
  * 找出矩阵中与0最近的距离，该元素有上下左右四个方向，
- * 思路：1.使用分治法比较笨;
- * 		2.采用动态规划比较合适，可以使用原矩阵记录上次的结果，具体保存该上次结果的思路是记录每个元素的上下左右最短距离，未计算标记为-1
+ * 思路：
+ * 1.使用BFS广度遍历，初始化矩阵：0入队，1改为最大，然后出队计算，以0节点向四个方向遍历，越界或者如果当前值v+1大于该方向上的值，则不做更新直接返回，否则更新该方向上的值并入队;
+ * 2.采用巧妙思路，先遍历上、左，然后倒序（上次错在没有倒序）遍历走下、右遍历更新该矩阵的值
  */
 public class Solution_542
 {
 	public int[][] updateMatrix(int[][] matrix)
 	{
+		//		return SolutionBFS(matrix);
+		return SolutionBest(matrix);
+	}
 
+	private int[][] SolutionBest(int[][] matrix)
+	{
+		int[][] rs = new int[matrix.length][matrix[0].length];
+		for (int[] r : rs)
+		{
+			Arrays.setAll(r, t -> {
+				return Integer.MAX_VALUE - 1;
+			});
+		}
+		for (int i = 0; i < matrix.length; i++)
+		{
+			for (int j = 0; j < matrix[i].length; j++)
+			{
+				if (matrix[i][j] == 0)
+				{
+					rs[i][j] = 0;
+				}
+				else
+				{
+					if (i > 0)
+					{
+						rs[i][j] = Math.min(rs[i][j], rs[i - 1][j] + 1);
+					}
+					if (j > 0)
+					{
+						rs[i][j] = Math.min(rs[i][j], rs[i][j - 1] + 1);
+					}
+				}
+			}
+		}
+		for (int i = matrix.length - 1; i >= 0; i--)
+		{
+			for (int j =matrix[i].length-1; j >=0; j--)
+			{
+
+				if (rs[i][j] == 0 || rs[i][j] == 1)
+				{
+					continue;
+				}
+				if (i < matrix.length - 1)
+				{
+					rs[i][j] = Math.min(rs[i][j], rs[i + 1][j] + 1);
+				}
+				if (j < matrix[i].length - 1)
+				{
+					rs[i][j] = Math.min(rs[i][j], rs[i][j + 1] + 1);
+				}
+
+			}
+		}
+		return rs;
+	}
+
+	private int[][] SolutionBFS(int[][] matrix)
+	{
 		Queue<int[]> queue = new LinkedList<>();
 		for (int i = 0; i < matrix.length; i++)
 		{
