425f1a4d161609e31457551ee8f0a418fd17cad7
liyongchao <liyongchao@corp.rrzcp8.com>
2018-05-15 11:46:04 +0800

    leetcode贪心算法-股票收益及种花

---
+++ src/main/java/com/lkzlee/leetcode/greedy/Solution_605.java
@@ -0,0 +1,50 @@
+package com.lkzlee.leetcode.greedy;
+
+/***
+ * @author:lkzlee
+ * @date: 2018/5/15 10:57
+ * @Desc:假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，
+ * 它们会争夺水源，两者都会死去。 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。
+ * 能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+ * 示例 1:
+ * 输入: flowerbed = [1,0,0,0,1], n = 1
+ * 输出: True
+ * 示例 2:
+ *
+ * 输入: flowerbed = [1,0,0,0,1], n = 2
+ * 输出: False
+ * 注意:
+ * 数组内已种好的花不会违反种植规则。
+ * 输入的数组长度范围为 [1, 20000]。
+ * n 是非负整数，且不会超过输入数组的大小。
+ */
+public class Solution_605
+{
+	public boolean canPlaceFlowers(int[] flowerbed, int n)
+	{
+		if (flowerbed == null)
+			return false;
+		if (n <= 0)
+			return true;
+		for (int i = 0; i < flowerbed.length; i++)
+		{
+			if (flowerbed[i] == 1)
+				continue;
+			int pre = (i == 0 ? 0 : flowerbed[i - 1]);
+			int next = ((i == flowerbed.length - 1) ? 0 : flowerbed[i + 1]);
+			if (pre == 0 && next == 0)
+			{
+				n--;
+				flowerbed[i] = 1;
+			}
+			if (n <= 0)
+				break;
+		}
+		return n <= 0;
+	}
+
+	public static void main(String[] args)
+	{
+
+	}
+}
