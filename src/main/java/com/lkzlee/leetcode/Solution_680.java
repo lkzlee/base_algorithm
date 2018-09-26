33d9850e57a4fa8a34769d594a513b7ce4ea39c3
lkzlee <liyongchao89@gmail.com>
2018-09-26 19:20:00 +0800

    双指针问题

--- src/main/java/com/lkzlee/leetcode/Solution_680.java
+++ src/main/java/com/lkzlee/leetcode/Solution_680.java
@@ -3,10 +3,16 @@ package com.lkzlee.leetcode;
 /***
  * @author:lkzlee
  * @date: 2018/9/26 19:01
- * @Desc:
- * Input: "abca"
- * Output: True
- * Explanation: You could delete the character 'c'.
+ * @Desc:给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+ * 示例 1:
+ * 输入: "aba"
+ * 输出: True
+ * 示例 2:
+ * 输入: "abca"
+ * 输出: True
+ * 解释: 你可以删除c字符。
+ * 注意:
+ * 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
  */
 public class Solution_680
 {
